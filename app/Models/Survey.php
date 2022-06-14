8c3b93f1f860290f8220ca0427bde4bdae88557d
francis-njoku <njokuchimauche@gmail.com>
2022-06-14 22:49:34 +0100

    Update Survey model

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -2,13 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+//trait 'Spatie\Sluggable\SlugOptions';
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Survey extends Model
 {
+    use HasFactory, HasSlug;
     // Table name
     protected $table = 'survey';
     // Primary Key
     public $primaryKey = 'id';
     // Timestamps
+
+    protected $fillable = ['user_id', 'title', 'slug', 'status', 'description', 'expire_date'];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+        ->generateSlugsFrom('title')
+        ->saveSlugsto('slug');  
+    }
 }
