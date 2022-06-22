61c47aa8e9096e5ac2d6c62625ed119650b9da7c
francis-njoku <njokuchimauche@gmail.com>
2022-06-22 04:46:07 +0100

    Add testign route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\SurveryController;
+use App\Http\Controllers\SurveyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +23,15 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::resource('/survey', \App\Http\Controllers\SurveryController::class);
+    Route::apiResource('/survey', SurveyController::class);
 });
 
+Route::get('/items', [ItemController::class, 'index']);
+Route::prefix('/item')->group( function () {
+    Route::post('/store', [ItemController::class, 'store']);
+    Route::put('/{id}', [ItemController::class, 'update']);
+    Route::delete('/{id}', [ItemController::class, 'delete']);
+});
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/sto', [ItemController::class, 'store2']);
