1bbc541719c5aa4a57780adae6bb691fa08bd24c
francis-njoku <njokuchimauche@gmail.com>
2022-06-22 04:44:17 +0100

    Update store request

--- app/Http/Requests/StoreSurveyRequest.php
+++ app/Http/Requests/StoreSurveyRequest.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Survey;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Enum;
 
 class StoreSurveyRequest extends FormRequest
 {
@@ -32,10 +36,13 @@ class StoreSurveyRequest extends FormRequest
     {
         return [
             'title' => 'required|string|max:1000',
-            'user_id' => 'exists:user,id',
+            'image' => 'nullable|string',
+            'user_id' => 'exists:users,id',
             'status' => 'required|boolean',
             'description' => 'nullable|string',
-            'expired_date' => 'nullable|date|after:tomorrow'
+            'expire_date' => 'nullable|date|after:tomorrow',
+            'questions' => 'array',
         ];
     }
+
 }
