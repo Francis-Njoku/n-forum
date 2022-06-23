e3321f0ba310c6c375a8b83c9f98f963076107d3
francis-njoku <njokuchimauche@gmail.com>
2022-06-23 06:34:43 +0100

    add image to vue

--- database/migrations/2022_06_22_034132_add_image_column_to_surveys_table.php
+++ database/migrations/2022_06_22_034132_add_image_column_to_surveys_table.php
@@ -14,7 +14,7 @@ class AddImageColumnToSurveysTable extends Migration
     public function up()
     {
         Schema::table('surveys', function (Blueprint $table) {
-            //
+            $table->string('image', 255)->nullable()->after('user_id');
         });
     }
 
@@ -26,7 +26,7 @@ class AddImageColumnToSurveysTable extends Migration
     public function down()
     {
         Schema::table('surveys', function (Blueprint $table) {
-            //
+            $table->dropColumn('image');
         });
     }
 }
