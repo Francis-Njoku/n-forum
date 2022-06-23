b46f203f76447c71722d7e46ba7e2f67eca93924
francis-njoku <njokuchimauche@gmail.com>
2022-06-23 06:34:30 +0100

    fix bugs on image view

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -6,11 +6,14 @@
     <template v-slot:header>
       <div class="flex items-center justify-between">
         <h1 class="text-3x1 font-bold text-gray-900">
-          {{ model.id ? model.title : "Create a Survey" }}
+          {{ route.params.id ? model.title : "Create a Survey" }}
         </h1>
+
+      <!-- Delete survey-->
       </div>
     </template>
-    <form @submit.prevent="saveSurvey">
+    <div v-if="surveyLoading" class="flex justify-center">Loading...</div>
+    <form v-else @submit.prevent="saveSurvey">
       <div class="shadow sm:rounded-md sm:overflow-hidden">
         <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
           <div>
@@ -190,7 +193,7 @@
 <script setup>
 import { v4 as uuidv4 } from "uuid";
 import store from "../store";
-import { ref } from "vue";
+import { ref, watch, computed } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
 import PageComponent from "../components/PageComponent.vue";
@@ -201,20 +204,34 @@ const router = useRouter();
 
 const route = useRoute();
 
+const surveyLoading = computed(() => store.state.currentSurvey.loading);
+
 // create empty survey
 let model = ref({
   title: "",
   status: false,
   description: null,
-  image: null,
+  image_url: null,
   expire_date: null,
   questions: [],
 });
 
+// Watch to current survey data change and when this happens we update local model
+watch(
+  () => store.state.currentSurvey.data,
+  (newVal, oldVal) => {
+    model.value = {
+      ...JSON.parse(JSON.stringify(newVal)),
+      status: newVal.status !== "draft",
+    };
+  }
+);
+
 if (route.params.id) {
-  model.value = store.state.surveys.find(
+  /*model.value = store.state.surveys.find(
     (s) => s.id === parseInt(route.params.id)
-  );
+  );*/
+  store.dispatch("getSurvey", route.params.id);
 }
 
 function onImageChoose(ev) {
