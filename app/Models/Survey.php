7210e4bf7a60264e7531d390e505cf19baa8389d
francis-njoku <njokuchimauche@gmail.com>
2022-06-26 15:28:18 +0100

    Add fillable question

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -17,6 +17,12 @@ class Survey extends Model
     //public $primaryKey = 'id';
     // Timestamps
 
+    const TYPE_TEXT = 'text';
+    const TYPE_TEXTAREA = 'textarea';
+    const TYPE_SELECT = 'select';
+    const TYPE_RADIO = 'radio';
+    const TYPE_CHECKBOX = 'checkbox'; 
+
     protected $fillable = ['user_id', 'image', 'title', 'slug', 'status', 'description', 'expire_date'];
 
     public function getSlugOptions(): SlugOptions
