3417ac6e5856c409a70c2db0c495cc8b6fcac2eb
francis-njoku <njokuchimauche@gmail.com>
2022-06-26 15:28:32 +0100

    Add Question function

--- app/Models/SurveyQuestion.php
+++ app/Models/SurveyQuestion.php
@@ -2,13 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SurveyQuestion extends Model
 {
     // Table name
-    protected $table = 'survey_questions';
+   // protected $table = 'survey_questions';
     // Primary Key
-    public $primaryKey = 'id';
+    //public $primaryKey = 'id';
     // Timestamps
+
+    use HasFactory;
+
+    protected $fillable = ['question', 'data', 'type', 'survey_id', 'description'];
 }
