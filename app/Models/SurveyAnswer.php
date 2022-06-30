c2bf23c4214048dc32e5c3e2637121dad4a8c7c5
francis-njoku <njokuchimauche@gmail.com>
2022-06-30 15:29:21 +0100

    Add fillable to survey answer

--- app/Models/SurveyAnswer.php
+++ app/Models/SurveyAnswer.php
@@ -2,13 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SurveyAnswer extends Model
 {
     // Table name
-    protected $table = 'survey_answers';
+    //protected $table = 'survey_answers';
     // Primary Key
-    public $primaryKey = 'id';
+    //public $primaryKey = 'id';
     // Timestamps
+
+    use HasFactory;
+
+    const CREATED_AT = null;
+    const UPDATED_AT = null;
+
+    protected $fillable = ['survey_id', 'start_date', 'end_date'];
 }
