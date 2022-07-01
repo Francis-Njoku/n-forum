08d06d0478eb32fec41ebf8ab558dd5e4975c2c7
francis-njoku <njokuchimauche@gmail.com>
2022-07-01 11:37:39 +0100

    Add Dashboard route to API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
@@ -24,6 +25,8 @@ Route::middleware('auth:sanctum')->group(function () {
     });
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('/survey', SurveyController::class);
+
+    Route::get('/dashboard', [DashboardController::class, 'index']);
 });
 
 Route::get('/items', [ItemController::class, 'index']);
