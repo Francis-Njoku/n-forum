8c253b60d08caa8856e0a2761d71ae4ebd789052
francis-njoku <njokuchimauche@gmail.com>
2022-07-01 11:38:29 +0100

    Add Answers belong to function

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -34,4 +34,9 @@ class Survey extends Model
     {
         return $this->hasMany(SurveyQuestion::class);
     }
+
+    public function answers() 
+    {
+        return $this->hasMany(SurveyAnswer::class);
+    }
 }
