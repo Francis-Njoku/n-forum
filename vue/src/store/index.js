481cba444b7656946461c2161e792c65241c9605
francis-njoku <njokuchimauche@gmail.com>
2022-07-02 01:37:23 +0100

    Include loading and sendData json

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -8,6 +8,10 @@ const store = createStore({
             data: {},
             token: sessionStorage.getItem("TOKEN"),
         },
+        dashboard: {
+            loading: false,
+            data: {},
+        },
         currentSurvey: {
             loading: false,
             data: {},
@@ -27,6 +31,20 @@ const store = createStore({
     },
     getters: {},
     actions: {
+        getDashboardData({ commit }) {
+            commit("dashboardLoading", true);
+            return axiosClient
+                .get("/dashboard")
+                .then((res) => {
+                    commit("dashboardLoading", false);
+                    commit("setDashboardData", res.data);
+                    return res;
+                })
+                .catch((error) => {
+                    commit("dashboardLoading", false);
+                    return error;
+                });
+        },
         getSurvey({ commit }, id) {
             commit("setCurrentSurveyLoading", true);
             return axiosClient
@@ -108,6 +126,12 @@ const store = createStore({
         },
     },
     mutations: {
+        dashboardLoading: (state, loading) => {
+            state.dashboard.loading = loading;
+        },
+        setDashboardData: (state, data) => {
+            state.dashboard.data = data;
+        },
         setCurrentSurveyLoading: (state, loading) => {
             state.currentSurvey.loading = loading;
         },
@@ -123,16 +147,16 @@ const store = createStore({
         },
         // these is for saving but it does  not render the image after saving/updating
         /*saveSurvey: (state, survey) => {
-                                        state.surveys = [...state.surveys, survey.data];
-                                    },
-                                    updateSurvey: (state, survey) => {
-                                        state.surveys = state.surveys.map((s) => {
-                                            if (s.id == survey.data.id) {
-                                                return survey.data;
-                                            }
-                                            return s;
-                                        });
-                                    },*/
+                                                        state.surveys = [...state.surveys, survey.data];
+                                                    },
+                                                    updateSurvey: (state, survey) => {
+                                                        state.surveys = state.surveys.map((s) => {
+                                                            if (s.id == survey.data.id) {
+                                                                return survey.data;
+                                                            }
+                                                            return s;
+                                                        });
+                                                    },*/
         logout: (state) => {
             state.user.data = {};
             state.user.token = null;
